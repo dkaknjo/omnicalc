9d2e628dce8666865a1dfec0b1a6361341431cc5
dkaknjo <Dino@dinos-mbp.attlocal.net>
2017-04-16 11:27:45 -0500

    some more edits

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -18,7 +18,7 @@ class CalculationsController < ApplicationController
 
     @character_count_without_spaces = @text.gsub(" ","").length
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = @special_word.split.length
 
     # ================================================================================
     # Your code goes above.
